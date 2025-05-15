336d24f6b043a6738a8b866feab0789d211f90df
Nguyen Do <khoanguyen.do@outlook.com>
2025-05-16 01:12:54 +0700

    init uv environment

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-create a folder 'preprocessed_images' under Dataset folder before running separate.py
+# Getting Started
+
+This project is managed by [uv](https://docs.astral.sh/uv/) to correctly resolve Python and its dependencies.
+
+To install the project dependencies, run
+
+```bash
+uv sync
+```
+
+Additionally, activate the Python virtual environment
+
+```bash
+uv venv
+source .venv/bin/activate
+```
+
+Start the Jupyter Notebook environment with
+
+```bash
+uv run --with jupyter jupyter lab
+```
+
+To start the project web server for getting predictions, run
+
+```bash
+uv run webapp.py
+```
+
+Here, uv ensure every run scripts has it dependencies resolve correctly and environment are self-contained.
+
+## References
+
+[Using uv with Jupyter](https://docs.astral.sh/uv/guides/integration/jupyter/)
--- flake.nix
+++ flake.nix
@@ -1,39 +1,50 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f rec {
           pkgs = import nixpkgs {inherit system;};
-          python = pkgs.python312;
+          python = pkgs.python311;
         });
   in {
+    # environment setup
     devShells = forEachSupportedSystem ({
       pkgs,
       python,
     }: {
       default = pkgs.mkShell {
-        venvDir = ".venv";
-        packages =
-          [python pkgs.pandoc pkgs.texliveFull pkgs.ruff]
-          ++ (with python.pkgs; [
-            pandas
-            numpy
-            scikit-learn
-            jupyter
-            notebook
-            matplotlib
-            seaborn
-            opencv4
-            tqdm
-            cleanvision
-            venvShellHook
-          ]);
+        packages = [
+          python
+          pkgs.uv
+
+          pkgs.nodejs
+          pkgs.pnpm
+        ];
+
+        venvDir = "./.venv";
+
+        env =
+          {
+            # Prevent uv from managing Python downloads
+            UV_PYTHON_DOWNLOADS = "never";
+            # Force uv to use nixpkgs Python interpreter
+            UV_PYTHON = python.interpreter;
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            # Python libraries often load native shared objects using dlopen(3).
+            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+          };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
       };
     });
   };
